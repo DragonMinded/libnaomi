612066830708ff53a912c4cb7feda116772c852d
Jennifer Taylor <dragonminded@dragonminded.com>
2021-12-17 19:46:44 +0000

    Add page system to sprite demo so we can start adding more features.

--- homebrew/examples/spritetest/main.c
+++ homebrew/examples/spritetest/main.c
@@ -1,6 +1,7 @@
 #include <naomi/video.h>
 #include <naomi/ta.h>
 #include <naomi/font.h>
+#include <naomi/maple.h>
 #include <naomi/sprite/sprite.h>
 
 #ifdef FEATURE_FREETYPE
@@ -9,14 +10,7 @@
 #define draw_text video_draw_text
 #endif
 
-// Display a white border around sprite locations.
-#define DISPLAY_GUIDEBOXES 0
-
-#if DISPLAY_GUIDEBOXES
-#define draw_guidebox(x0, y0, x1, y1, color) sprite_draw_box(x0, y0, x1, y1, color)
-#else
-#define draw_guidebox(x0, y0, x1, y1, color) do { } while(0)
-#endif
+#define MAXPAGES 1
 
 #define WPAD (crate_png_width + 2)
 #define HPAD (crate_png_height + 2)
@@ -34,6 +28,14 @@ void main()
     font_t *font = font_add(dejavusans_ttf_data, dejavusans_ttf_len);
     font_set_size(font, 12);
 #endif
+
+    /* Load page arrows into VRAM. */
+    extern uint8_t *arrowlt_png_data;
+    extern unsigned int arrowlt_png_width;
+    texture_description_t *leftarrow = ta_texture_desc_malloc_direct(arrowlt_png_width, arrowlt_png_data, TA_TEXTUREMODE_ARGB4444);
+    extern uint8_t *arrowrt_png_data;
+    extern unsigned int arrowrt_png_width;
+    texture_description_t *rightarrow = ta_texture_desc_malloc_direct(arrowrt_png_width, arrowrt_png_data, TA_TEXTUREMODE_ARGB4444);
 
     /* Load our crate texture into VRAM. Note that the image is purposefully square and a multiple
      * of 2 with a minimum value of 8x8. */
@@ -65,59 +67,104 @@ void main()
 
     /* Display sprite demo. */
     int tickcount = 0;
+    int page = 0;
     while (1)
     {
+        /* Listen to inputs so we can change pages */
+        maple_poll_buttons();
+        jvs_buttons_t buttons = maple_buttons_pressed();
+
+        if (buttons.player1.left)
+        {
+            if (page == 0)
+            {
+                page = MAXPAGES - 1;
+            }
+            else
+            {
+                page--;
+            }
+        }
+        else if (buttons.player1.right)
+        {
+            if (page == (MAXPAGES - 1))
+            {
+                page = 0;
+            }
+            else
+            {
+                page++;
+            }
+        }
+
         /* Begin sending commands to the TA to draw stuff */
         ta_commit_begin();
 
-        /* Simple and 90 degree sprite display. */
-        draw_text(64, 46, font, rgb(255, 255, 255), "Simple, non-rotated as well as rotated sprites:");
+        switch (page)
+        {
+            case 0:
+            {
+                /* Simple and 90 degree sprite display. */
+                draw_text(64, 46, font, rgb(255, 255, 255), "Simple, non-rotated as well as rotated sprites:");
 
-        /* Simple sprite */
-        draw_guidebox(63, 63, 63 + WPAD, 63 + HPAD, rgb(255, 255, 255));
-        sprite_draw_simple(64, 64, crate);
+                /* Simple sprite */
+                sprite_draw_simple(64, 64, crate);
 
-        /* Rotated sprites */
-        draw_guidebox(143, 63, 143 + WPAD, 63 + HPAD, rgb(255, 255, 255));
-        sprite_draw_rotated(144, 64, 90, crate);
-        draw_guidebox(223, 63, 223 + WPAD, 63 + HPAD, rgb(255, 255, 255));
-        sprite_draw_rotated(224, 64, 180, crate);
-        draw_guidebox(303, 63, 303 + WPAD, 63 + HPAD, rgb(255, 255, 255));
-        sprite_draw_rotated(304, 64, 270, crate);
+                /* Rotated sprites */
+                sprite_draw_rotated(144, 64, 90, crate);
+                sprite_draw_rotated(224, 64, 180, crate);
+                sprite_draw_rotated(304, 64, 270, crate);
 
-        draw_guidebox(389, 63, 389 + WPAD, 63 + HPAD, rgb(255, 255, 255));
-        sprite_draw_rotated(390, 64, tickcount % 360, crate);
+                /* Arbitrarily rotated sprite. */
+                sprite_draw_rotated(390, 64, tickcount % 360, crate);
 
-        /* Odd shaped (non-square) sprites. */
-        draw_text(64, 138, font, rgb(255, 255, 255), "Odd-shaped (non-square) sprites,\nnormal and rotated:");
+                /* Odd shaped (non-square) sprites. */
+                draw_text(64, 138, font, rgb(255, 255, 255), "Odd-shaped (non-square) sprites,\nnormal and rotated:");
 
-        /* Simple odd sprite */
-        sprite_draw_nonsquare(64, 172, sonic_png_width, sonic_png_height, sonic);
+                /* Simple odd sprite */
+                sprite_draw_nonsquare(64, 172, sonic_png_width, sonic_png_height, sonic);
 
-        /* Rotated odd sprite */
-        sprite_draw_nonsquare_rotated(150, 172, sonic_png_width, sonic_png_height, 360 - (tickcount % 360), sonic);
+                /* Rotated odd sprite */
+                sprite_draw_nonsquare_rotated(150, 172, sonic_png_width, sonic_png_height, 360 - (tickcount % 360), sonic);
 
-        /* Transparency example */
-        draw_text(280, 146, font, rgb(255, 255, 255), "Proper alpha blending:");
+                /* Transparency example */
+                draw_text(280, 146, font, rgb(255, 255, 255), "Proper alpha blending:");
 
-        /* Draw two sprites overlappint */
-        sprite_draw_simple(280, 172 + sonic_png_height - crate_png_height, crate);
-        sprite_draw_nonsquare(295, 172, sonic_png_width, sonic_png_height, sonic);
+                /* Draw two sprites overlappint */
+                sprite_draw_simple(280, 172 + sonic_png_height - crate_png_height, crate);
+                sprite_draw_nonsquare(295, 172, sonic_png_width, sonic_png_height, sonic);
 
-        /* Tilemaps (easy animation, 2D maps, etc). */
-        draw_text(64, 270, font, rgb(255, 255, 255), "Tilemap support, both normal and rotated:");
+                /* Tilemaps (easy animation, 2D maps, etc). */
+                draw_text(64, 270, font, rgb(255, 255, 255), "Tilemap support, both normal and rotated:");
 
-        /* Draw animated coins, with full animation cycle every 30 frames. */
-        sprite_draw_tilemap_entry(64, 288, 32, (tickcount / 6) % 5, coins);
-        sprite_draw_tilemap_entry(100, 288, 32, (((tickcount / 6) + 1) % 5) + 5, coins);
-        sprite_draw_tilemap_entry(134, 288, 32, (((tickcount / 6) + 3) % 5) + 10, coins);
+                /* Draw animated coins, with full animation cycle every 30 frames. */
+                sprite_draw_tilemap_entry(64, 288, 32, (tickcount / 6) % 5, coins);
+                sprite_draw_tilemap_entry(100, 288, 32, (((tickcount / 6) + 1) % 5) + 5, coins);
+                sprite_draw_tilemap_entry(134, 288, 32, (((tickcount / 6) + 3) % 5) + 10, coins);
 
-        /* Draw animated gems, with full cycle animation cycle every 60 frames. */
-        sprite_draw_tilemap_entry_rotated(180, 288, 32, (tickcount / 15) % 4, (tickcount * 9) % 360, color_gems);
-        sprite_draw_tilemap_entry_rotated(220, 288, 32, ((tickcount / 15) % 4) + 4, (tickcount * 9) % 360 + 60, color_gems);
-        sprite_draw_tilemap_entry_rotated(260, 288, 32, ((tickcount / 15) % 4) + 8, (tickcount * 9) % 360 + 120, color_gems);
-        sprite_draw_tilemap_entry_rotated(300, 288, 32, ((tickcount / 15) % 4) + 12, (tickcount * 9) % 360 + 180, color_gems);
-        sprite_draw_tilemap_entry_rotated(340, 288, 32, (tickcount / 15) % 4, (tickcount * 9) % 360 + 240, gray_gem);
+                /* Draw animated gems, with full cycle animation cycle every 60 frames. */
+                sprite_draw_tilemap_entry_rotated(180, 288, 32, (tickcount / 15) % 4, (tickcount * 9) % 360, color_gems);
+                sprite_draw_tilemap_entry_rotated(220, 288, 32, ((tickcount / 15) % 4) + 4, (tickcount * 9) % 360 + 60, color_gems);
+                sprite_draw_tilemap_entry_rotated(260, 288, 32, ((tickcount / 15) % 4) + 8, (tickcount * 9) % 360 + 120, color_gems);
+                sprite_draw_tilemap_entry_rotated(300, 288, 32, ((tickcount / 15) % 4) + 12, (tickcount * 9) % 360 + 180, color_gems);
+                sprite_draw_tilemap_entry_rotated(340, 288, 32, (tickcount / 15) % 4, (tickcount * 9) % 360 + 240, gray_gem);
+
+                break;
+            }
+        }
+
+        /* Draw page markers at the bottom */
+        sprite_draw_box(32 + 8, video_height() - 32 + 8, video_width() - 32 + 8, video_height() - 64 + 8, rgb(90, 90, 90));
+        sprite_draw_box(32 + 4, video_height() - 32 + 4, video_width() - 32 + 4, video_height() - 64 + 4, rgb(180, 180, 180));
+        sprite_draw_box(32, video_height() - 32, video_width() - 32, video_height() - 64, rgb(255, 255, 255));
+
+        char instructions[] = "press arrow keys to change demo screen";
+        font_metrics_t metrics = font_get_text_metrics(font, instructions);
+        int instwidth = metrics.width + 40;
+
+        sprite_draw_simple((video_width() - instwidth) / 2, video_height() - 64 + 8, leftarrow);
+        sprite_draw_simple(((video_width() - instwidth) / 2) + 20, video_height() - 64 + 8, rightarrow);
+        draw_text(((video_width() - instwidth) / 2) + 40, video_height() - 64 + 8, font, rgb(32, 32, 32), instructions);
 
         /* Mark the end of the command list */
         ta_commit_end();
