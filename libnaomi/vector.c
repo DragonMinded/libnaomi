a708743557b9e8cf8f294f0835afcdb8d1e2e984
Jennifer Taylor <dragonminded@dragonminded.com>
2021-12-27 16:36:59 +0000

    Get rid of software SQRT in vectors, use SH4 inner product instructions to drastically speed up dot product, length and normalize.

--- libnaomi/vector.c
+++ libnaomi/vector.c
@@ -1,10 +1,29 @@
 #include "naomi/vector.h"
 #include "naomi/matrix.h"
-#include <math.h>
+#include "naomi/interrupt.h"
 
 float vector_length(vector_t *vec)
 {
-    return sqrt((vec->x * vec->x) + (vec->y * vec->y) + (vec->z * vec->z));
+    uint32_t old_irq = irq_disable();
+
+    register vector_t *vec_param asm("r4") = vec;
+    register float retval asm("fr0");
+    asm volatile(" \
+        fmov.s @r4+,fr0\n \
+        fmov.s @r4+,fr1\n \
+        fmov.s @r4+,fr2\n \
+        fldi0 fr3\n \
+        fipr fv0,fv0\n \
+        fsqrt fr3\n \
+        fmov fr3,fr0\n \
+        " :
+        "=r" (retval) :
+        "r" (vec_param) :
+        "fr1", "fr2", "fr3"
+    );
+
+    irq_restore(old_irq);
+    return retval;
 }
 
 void vector_from_vertex(vector_t *vec, vertex_t *first, vertex_t *second)
@@ -30,7 +49,30 @@ void vector_cross(vector_t *cross, vector_t *a, vector_t *b)
 
 float vector_dot(vector_t *a, vector_t *b)
 {
-    return (a->x * b->x) + (a->y * b->y) + (a->z * b->z);
+    uint32_t old_irq = irq_disable();
+
+    register vector_t *a_param asm("r4") = a;
+    register vector_t *b_param asm("r5") = b;
+    register float retval asm("fr0");
+    asm volatile(" \
+        fmov.s @r4+,fr0\n \
+        fmov.s @r4+,fr1\n \
+        fmov.s @r4+,fr2\n \
+        fldi0 fr3\n \
+        fmov.s @r5+,fr4\n \
+        fmov.s @r5+,fr5\n \
+        fmov.s @r5+,fr6\n \
+        fldi0 fr7\n \
+        fipr fv4,fv0\n \
+        fmov fr3,fr0\n \
+        " :
+        "=r" (retval) :
+        "r" (a_param), "r" (b_param) :
+        "fr1", "fr2", "fr3", "fr4", "fr5", "fr6", "fr7"
+    );
+
+    irq_restore(old_irq);
+    return retval;
 }
 
 void vector_normalize(vector_t *vec)
