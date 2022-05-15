1e32db7c6485b0c96e5be99f00e2c89e71083fa8
Jennifer Taylor <dragonminded@dragonminded.com>
2022-05-15 16:52:50 +0000

    Add a note about dangerous functions in interrupt.h and alternatives to use in their stead.

--- libnaomi/naomi/interrupt.h
+++ libnaomi/naomi/interrupt.h
@@ -9,12 +9,25 @@ extern "C" {
 
 // Ensure interrupts are disabled, returning the old SR. When you are done with
 // the code that needs exclusive HW access, restore interrupts with irq_restore().
+// Note that under most circumstances you should not use this in your code. It is
+// much safer to instead guard code with a mutex. Using irq_disable()/irq_restore()
+// without extreme care can cause system code which might need to make syscalls to
+// crash.
 uint32_t irq_disable();
 
-// Restore interrupts, after calling irq_disable().
+// Restore interrupts, after calling irq_disable(). Note that you should always have
+// an irq_restore() call matching an irq_disable() call, otherwise you will leave
+// interrupts disabled permanently and many parts of the system will no longer work.
 void irq_restore(uint32_t oldstate);
 
-// Run an enclosed statement atomically.
+// Run an enclosed statement or group of statements atomically. The most common place
+// where you might use this is for wrapping C stdlib calls such as sprintf in order
+// to make them threadsafe with respect to common buffers. In most circumstances you
+// should probably use a mutex instead, but this handy macro can save you a bit of
+// setup and teardown. Note that you can include a block of statements by replacing
+// the single statement by angle brackets. If you do this, do note that return
+// statements inside that code are not safe! This appears to work similar to a
+// resource guard in modern languages, but C doesn't have finally support!
 #define ATOMIC(stmt) \
 do { \
     uint32_t old_ints = irq_disable(); \
@@ -53,6 +66,8 @@ typedef struct
 #define IRQ_EVENT_TMU1 0x420
 #define IRQ_EVENT_TMU2 0x440
 
+// Grab interrupt statistics. This is mostly for debugging and doesn't contain the level
+// of detail necessary to make much use of it.
 irq_stats_t irq_stats();
 
 #ifdef __cplusplus
